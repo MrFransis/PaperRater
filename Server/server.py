9e27ed0a3c412738827d1dc67761faa9613bc6b1
edoardoruffoli <ruffoli99@gmail.com>
2021-12-10 09:55:01 +0100

    updated server.py

--- Server/server.py
+++ Server/server.py
@@ -1,68 +1,66 @@
+import cmd
+import sys
+from asyncio import sleep
+from pymongo import MongoClient
 import pandas as pd
 import random
 from datetime import datetime
 
-import getPapers
-import getUsers
 
-
-def uploadData(path, collection):
-    data = pd.read_json(path, lines=True)
-    data_dict = data.to_dict("records")
-    collection.insert_many(data_dict)
-
-def uploadComments():
-    from pymongo import MongoClient
-    client = MongoClient('localhost', 27017)
-
-    db = client["PaperRater"]
-    papers_col = db["Papers"]
-
-    # Users dataframe
-    users_df = pd.read_json('./data/users.json', lines=True)
-
-    for paper in papers_col.find():
-        num_comments = int(random.random()*10)
-        comments = []
-        for i in range(0, num_comments):
-            now = datetime.now()
-
-            comment = {'username': users_df.sample()['username'].values[0],
-                       'comment': "Commento",   # getRandom Comment
-                       'timestamp': now.strftime("%Y-%m-%d %H:%M:%S")}
-            comments.append(comment)
-
-        result = db.Papers.update_one({'_id': paper['_id']}, {'$set': {'comments': comments}})
-
-
-def init(papers_path, users_path):
-    from pymongo import MongoClient
-    client = MongoClient('localhost', 27017)
-    client.drop_database('PaperRater')
-
-    db = client["PaperRater"]
-
-    papers_col = db["Papers"]
-    users_col = db["Users"]
-
-    uploadData(papers_path, papers_col)
-    print("Added papers to database")
-
-    uploadData(users_path, users_col)
-    print("Added users to database")
-
-    uploadComments()
-    print("Added comments to database")
-
-if __name__ == "__main__":
+class App(cmd.Cmd):
+    intro = 'PaperRater Server launched. \n \nType help or ? to list commands.\n'
+    prompt = '>'
 
     start_date = '2021-11-30'
     num_users = '1000'
+    client = MongoClient('localhost', 27017)
+    # papers_path = getPapers.import_data(start_date)
+    papers_path = './data/papers2021-11-30.json'
+    # users_path = getUsers.import_data(num_users)
+    users_path = './data/users.json'
 
-    papers_path = getPapers.import_data(start_date)
-    users_path = getUsers.import_data(num_users)
 
-    init(papers_path, users_path)
+    def do_initDB(self, arg):
+        'Initialize the database'
+        # Drop old database
+        self.client.drop_database('PaperRater')
 
-   # while True:
-        # daily update routine
+        db = self.client["PaperRater"]
+
+        ### Papers
+        papers_col = db["Papers"]
+        data = pd.read_json(self.papers_path, lines=True)
+        data_dict = data.to_dict("records")
+        papers_col.insert_many(data_dict)
+        print("Added papers to database")
+
+        ### Users
+        users_col = db["Users"]
+        users_df = pd.read_json(self.users_path, lines=True)
+        data_dict = users_df.to_dict("records")
+        users_col.insert_many(data_dict)
+        print("Added users to database")
+
+        ### Comments
+        for paper in papers_col.find():
+            num_comments = int(random.random() * 10)
+            comments = []
+            for i in range(0, num_comments):
+                now = datetime.now()
+
+                comment = {'username': users_df.sample()['username'].values[0],
+                           'comment': "Commento",  # getRandom Comment
+                           'timestamp': now.strftime("%Y-%m-%d %H:%M:%S")}
+                comments.append(comment)
+
+            result = db.Papers.update_one({'_id': paper['_id']}, {'$set': {'comments': comments}})
+
+        print("Added comments to database")
+
+
+    def do_exit(self, arg):
+        'Exit PaperRater Server'
+        sys.exit()
+
+if __name__ == '__main__':
+    App().cmdloop()
