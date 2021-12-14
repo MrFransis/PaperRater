cd98a465e4f8515e2c9606f0aa5b87966889c7b7
MrFransis <franceh96@hotmail.it>
2021-12-14 11:32:59 +0100

    Create driver for MongoDB

--- PaperRaterApp/src/main/java/it/unipi/dii/lsmd/paperraterapp/persistence/MongoDriver.java
+++ PaperRaterApp/src/main/java/it/unipi/dii/lsmd/paperraterapp/persistence/MongoDriver.java
@@ -3,6 +3,8 @@ package it.unipi.dii.lsmd.paperraterapp.persistence;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.client.*;
+import it.unipi.dii.lsmd.paperraterapp.config.ConfigurationParameters;
+import it.unipi.dii.lsmd.paperraterapp.utils.Utils;
 import org.bson.Document;
 
 import java.util.function.Consumer;
@@ -10,10 +12,26 @@ import java.util.function.Consumer;
 public class MongoDriver implements DatabaseDriver {
     private static MongoDriver instance;
 
-    private static MongoClient client = null;
-    private static MongoDatabase database = null;
+    private MongoClient client = null;
+    private MongoDatabase database = null;
     private MongoCollection collection;
+    public String firstIp;
+    public int firstPort;
+    public String dbName;
 
+    private MongoDriver(ConfigurationParameters configurationParameters){
+        this.firstIp = configurationParameters.getMongoFirstIp();
+        this.firstPort = configurationParameters.getMongoFirstPort();
+        this.dbName = configurationParameters.getMongoDbName();
+    };
+
+    public static MongoDriver getInstance() {
+        if (instance == null)
+        {
+            instance = new MongoDriver(Utils.readConfigurationParameters());
+        }
+        return instance;
+    }
     /**
      * Method that inits the MongoClient and choose the correct database
      */
