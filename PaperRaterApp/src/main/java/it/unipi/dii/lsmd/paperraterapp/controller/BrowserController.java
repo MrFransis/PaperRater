fc773b21103ac40f9bd14fef53f456d06b324fa3
balditommaso <84393995+balditommaso@users.noreply.github.com>
2022-01-03 18:17:01 +0100

    working on special query

--- PaperRaterApp/src/main/java/it/unipi/dii/lsmd/paperraterapp/controller/BrowserController.java
+++ PaperRaterApp/src/main/java/it/unipi/dii/lsmd/paperraterapp/controller/BrowserController.java
@@ -59,18 +59,7 @@ public class BrowserController implements Initializable {
     private Neo4jManager neo4jManager;
     private User user;
     private int page;
-
-    @Override
-    public void initialize (URL url, ResourceBundle resourceBundle) {
-        mongoManager = new MongoDBManager(MongoDriver.getInstance().openConnection());
-        neo4jManager = new Neo4jManager(Neo4jDriver.getInstance().openConnection());
-        user = Session.getInstance().getLoggedUser();
-        loadComboBox();
-        hideFilterForm();
-        forwardBt.setOnMouseClicked(mouseEvent -> goForward());
-        backBt.setOnMouseClicked(mouseEvent -> goBack());
-        logoutLabel.setOnMouseClicked(mouseEvent -> logout(mouseEvent));
-    }
+    private boolean special;
 
     @FXML
     void goToProfilePage(MouseEvent event) {
@@ -80,11 +69,31 @@ public class BrowserController implements Initializable {
     }
 
     @FXML
-    void activeResearch() {
+    private void logout(MouseEvent event) {
+        Session.resetInstance();
+        Utils.changeScene("/it/unipi/dii/lsmd/paperraterapp/layout/login.fxml", event);
+    }
+
+    @Override
+    public void initialize (URL url, ResourceBundle resourceBundle) {
+        mongoManager = new MongoDBManager(MongoDriver.getInstance().openConnection());
+        neo4jManager = new Neo4jManager(Neo4jDriver.getInstance().openConnection());
+        user = Session.getInstance().getLoggedUser();
+        special = false;
+        loadComboBox();
+        hideFilterForm();
+        forwardBt.setOnMouseClicked(mouseEvent -> goForward());
+        backBt.setOnMouseClicked(mouseEvent -> goBack());
+    }
+
+    // -------------------------------------------- NORMAL RESEARCH --------------------------------------------
+    @FXML
+    void switchForm() {
         searchBt.setDisable(false);
         backBt.setDisable(true);
         forwardBt.setDisable(true);
         followsCheckBox.setSelected(false);
+        special = false;
         switch (chooseType.getValue()) {
             case "Papers" -> {
                 authorContainer.setVisible(true);
@@ -128,11 +137,113 @@ public class BrowserController implements Initializable {
         forwardBt.setDisable(false);
         backBt.setDisable(true);
         page = 0;
+        special = false;
         handleResearch();
     }
 
+    private void handleResearch() {
+        special = false;
+        switch (chooseType.getValue()) {
+            case "Papers" -> {
+                // check the form values
+                errorTf.setText("");
+                if (keywordTf.getText().equals("") && toDate.getValue() == null && fromDate.getValue() == null &&
+                        (chooseCategory.getValue() == null || chooseCategory.getValue().equals("Select category"))
+                        && authorTf.getText().equals("")) {
+                    errorTf.setText("You have to set some filters.");
+                    forwardBt.setDisable(true);
+                    return;
+                }
+                if (toDate.getValue() != null && fromDate.getValue() != null &&
+                        toDate.getValue().isBefore(fromDate.getValue())) {
+                    errorTf.setText("The From date have to be before the To date.");
+                    forwardBt.setDisable(true);
+                    return;
+                }
+                String category = "";
+                String startDate = "";
+                String endDate = "";
+                if (chooseCategory.getValue() != null && !chooseCategory.getValue().equals("Select category"))
+                    category = chooseCategory.getValue();
+                if (toDate.getValue() != null)
+                    endDate = toDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                if (fromDate.getValue() != null)
+                    startDate = fromDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                // handle cards display
+                // load papers
+                List<Paper> papersList = mongoManager.searchPapersByParameters(keywordTf.getText(), authorTf.getText(),
+                        startDate, endDate, category, 3*page, 3);
+                fillPapers(papersList);
+            }
+            case "Users" -> {
+                // form control
+                errorTf.setText("");
+                if (keywordTf.getText().equals("") && !followsCheckBox.isSelected()) {
+                    errorTf.setText("You have to specify an option.");
+                    return;
+                }
+                List<User> usersList = null;
+                // check if you need follows
+                if(followsCheckBox.isSelected())
+                    usersList = neo4jManager.getSnapsOfFollowedUserByKeyword(user, keywordTf.getText(), 8, 8*page);
+                else
+                    usersList = mongoManager.getUsersByKeyword(keywordTf.getText(), false, page);
+                fillUsers(usersList);
+            }
+            case "Reading lists" -> {
+                // form control
+                errorTf.setText("");
+                if (keywordTf.getText().equals("") && !followsCheckBox.isSelected()) {
+                    errorTf.setText("You have to specify an option.");
+                    return;
+                }
+                // load papers
+                List<Pair<String, ReadingList>> readingLists = null;
+                if (followsCheckBox.isSelected())
+                    readingLists = neo4jManager.getSnapsOfFollowedReadingListsByKeyword(keywordTf.getText(), user, 4*page, 4);
+                else
+                    readingLists = mongoManager.getReadingListByKeywords(keywordTf.getText(), 4*page, 4);
+                fillReadingLists(readingLists);
+            }
+            case "Moderate comments" -> {
+                // form control
+                errorTf.setText("");
+                if (toDate.getValue() != null && fromDate.getValue() != null &&
+                        toDate.getValue().isBefore(fromDate.getValue())) {
+                    errorTf.setText("The From date have to be before the To date.");
+                    forwardBt.setDisable(true);
+                    return;
+                }
+
+                String startDate = "";
+                String endDate = "";
+
+                if (toDate.getValue() != null)
+                    endDate = toDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                if (fromDate.getValue() != null)
+                    startDate = fromDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+                fillComments(startDate, endDate);
+            }
+            case "Search moderator" -> {
+                List<User> usersList = mongoManager.getUsersByKeyword(keywordTf.getText(), true, page);
+                fillUsers(usersList);
+            }
+        }
+    }
+
+    // -------------------------------------------- SPECIAL RESEARCH --------------------------------------------
     @FXML
     void showOption() {
+        if (chooseQuery.getValue().equals("Special research")) {
+            cleanGrid();
+            paramContainer.setVisible(false);
+            timeRangeContainer.setVisible(false);
+            chooseTarget.getItems().clear();
+            chooseTimeRange.getItems().clear();
+            special = false;
+            return;
+        }
         List<String> typeList = new ArrayList<>();
         typeList.add("Papers");
         typeList.add("Users");
@@ -141,48 +252,14 @@ public class BrowserController implements Initializable {
         chooseTarget.getItems().clear();
         chooseTarget.setItems(observableListType);
         paramContainer.setVisible(true);
+        special = true;
     }
 
     @FXML
     void firstSelection() {
         switch (chooseQuery.getValue()) {
             case "Suggestion" -> {
-                switch (chooseTarget.getValue()) {
-                    case "Users" -> {
-                        setGridUsers();
-                        List<User> suggestedUser = neo4jManager.getSnapsOfSuggestedUsers(user, 4, 4);
-                        int row = 0;
-                        int col = 0;
-                        for (User u : suggestedUser) {
-                            Pane card = loadUsersCard(u);
-                            cardsGrid.add(card, col, row);
-                            col++;
-                            if (col == 4) {
-                                col = 0;
-                                row++;
-                            }
-                        }
-                    }
-                    case "Papers" -> {
-                        setGridPapers();
-                        List<Paper> suggestedPaper = neo4jManager.getSnapsOfSuggestedPapers(user, 2, 1);
-                        int row = 0;
-                        for (Paper p : suggestedPaper) {
-                            Pane card = loadPapersCard(p);
-                            cardsGrid.add(card, 0, row);
-                            row++;
-                        }
-                    }
-                    case "Reading lists" -> {
-                        List<Pair<String, ReadingList>> suggestedReadingLists = neo4jManager.getSnapsOfSuggestedReadingLists(user, 2, 2);
-                        int row = 0;
-                        for (Pair<String, ReadingList> cardInfo : suggestedReadingLists) {
-                            Pane card = loadReadingListsCard(cardInfo.getValue(), cardInfo.getKey());
-                            cardsGrid.add(card, 0, row);
-                            row++;
-                        }
-                    }
-                }
+                secondSelection();
             }
             case "Analytics" -> {
 
@@ -197,19 +274,33 @@ public class BrowserController implements Initializable {
                 chooseTimeRange.setItems(observableListType);
                 timeRangeContainer.setVisible(true);
             }
-            default -> {
-
-            }
         }
-
     }
 
     @FXML
     void secondSelection() {
-
+        special = true;
+        forwardBt.setVisible(true);
+        switch (chooseQuery.getValue()) {
+            case "Suggestion" -> {
+                switch (chooseTarget.getValue()) {
+                    case "Users" -> {
+                        List<User> suggestedUser = neo4jManager.getSnapsOfSuggestedUsers(user, 20, 20);
+                        fillUsers(suggestedUser);
+                    }
+                    case "Papers" -> {
+                        List<Paper> suggestedPaper = neo4jManager.getSnapsOfSuggestedPapers(user, 20, 10);
+                        fillPapers(suggestedPaper);
+                    }
+                    case "Reading lists" -> {
+                        List<Pair<String, ReadingList>> suggestedReadingLists = neo4jManager.getSnapsOfSuggestedReadingLists(user, 20, 20);
+                        fillReadingLists(suggestedReadingLists);
+                    }
+                }
+            }
+        }
     }
-
-
+    // -------------------------------------------- UTILS --------------------------------------------
 
     private Pane loadUsersCard (User user) {
         Pane pane = null;
@@ -270,6 +361,7 @@ public class BrowserController implements Initializable {
     private void loadComboBox () {
         // load suggestion
         List<String> suggestionList = new ArrayList<>();
+        suggestionList.add("Special research");
         suggestionList.add("Suggestion");
         suggestionList.add("Analytics");
         suggestionList.add("Summary");
@@ -306,17 +398,9 @@ public class BrowserController implements Initializable {
         followsContainer.setVisible(false);
     }
 
-    private void fillUsers(String keyword, boolean moderator) {
+    private void fillUsers(List<User> usersList) {
         // set new layout
         setGridUsers();
-        // load users
-        List<User> usersList = null;
-        // check if you need follows
-        if(followsCheckBox.isSelected())
-            usersList = neo4jManager.getSnapsOfFollowedUserByKeyword(user, keyword, 8, 8*page);
-        else
-            usersList = mongoManager.getUsersByKeyword(keyword, moderator, page);
-
         if (usersList.size() != 8)
             forwardBt.setDisable(true);
         int row = 0;
@@ -332,11 +416,8 @@ public class BrowserController implements Initializable {
         }
     }
 
-    private void fillPapers(String title, String author, String start_date, String end_date, String category) {
+    private void fillPapers(List<Paper> papersList) {
         setGridPapers();
-        // load papers
-        List<Paper> papersList = mongoManager.searchPapersByParameters(title, author, start_date, end_date, category,
-                3*page, 3);
         if (papersList.size() != 3)
             forwardBt.setDisable(true);
         int row = 0;
@@ -347,14 +428,8 @@ public class BrowserController implements Initializable {
         }
     }
 
-    private void fillReadingLists(String keyword) {
+    private void fillReadingLists(List<Pair<String, ReadingList>> readingLists) {
         setGridReadingList();
-        // load papers
-        List<Pair<String, ReadingList>> readingLists = null;
-        if (followsCheckBox.isSelected())
-            readingLists = neo4jManager.getSnapsOfFollowedReadingListsByKeyword(keyword, user, 4*page, 4);
-        else
-            readingLists = mongoManager.getReadingListByKeywords(keyword, 4*page, 4);
         if (readingLists.size() != 4)
             forwardBt.setDisable(true);
         int row = 0;
@@ -391,87 +466,14 @@ public class BrowserController implements Initializable {
         }
     }
 
-    private void handleResearch() {
-        switch (chooseType.getValue()) {
-            case "Papers" -> {
-                // check the form values
-                errorTf.setText("");
-                if (keywordTf.getText().equals("") && toDate.getValue() == null && fromDate.getValue() == null &&
-                        (chooseCategory.getValue() == null || chooseCategory.getValue().equals("Select category"))
-                        && authorTf.getText().equals("")) {
-                    errorTf.setText("You have to set some filters.");
-                    forwardBt.setDisable(true);
-                    return;
-                }
-                if (toDate.getValue() != null && fromDate.getValue() != null &&
-                        toDate.getValue().isBefore(fromDate.getValue())) {
-                    errorTf.setText("The From date have to be before the To date.");
-                    forwardBt.setDisable(true);
-                    return;
-                }
-                String category = "";
-                String startDate = "";
-                String endDate = "";
-                if (chooseCategory.getValue() != null && !chooseCategory.getValue().equals("Select category"))
-                    category = chooseCategory.getValue();
-                if (toDate.getValue() != null)
-                    endDate = toDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                if (fromDate.getValue() != null)
-                    startDate = fromDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                // handle cards display
-                fillPapers(keywordTf.getText(), authorTf.getText(), startDate, endDate, category);
-            }
-            case "Users" -> {
-                // form control
-                errorTf.setText("");
-                if (keywordTf.getText().equals("") && !followsCheckBox.isSelected()) {
-                    errorTf.setText("You have to specify an option.");
-                    return;
-                }
-                fillUsers(keywordTf.getText(), false);
-            }
-            case "Reading lists" -> {
-                // form control
-                errorTf.setText("");
-                if (keywordTf.getText().equals("") && !followsCheckBox.isSelected()) {
-                    errorTf.setText("You have to specify an option.");
-                    return;
-                }
-                fillReadingLists(keywordTf.getText());
-            }
-            case "Moderate comments" -> {
-                // form control
-                errorTf.setText("");
-                if (toDate.getValue() != null && fromDate.getValue() != null &&
-                        toDate.getValue().isBefore(fromDate.getValue())) {
-                    errorTf.setText("The From date have to be before the To date.");
-                    forwardBt.setDisable(true);
-                    return;
-                }
-
-                String startDate = "";
-                String endDate = "";
-
-                if (toDate.getValue() != null)
-                    endDate = toDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                if (fromDate.getValue() != null)
-                    startDate = fromDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
-                fillComments(startDate, endDate);
-            }
-            case "Search moderator" -> {
-                // form control
-                errorTf.setText("");
-
-                fillUsers(keywordTf.getText(), true);
-            }
-        }
-    }
-
     private void goForward () {
         page++;
         backBt.setDisable(false);
         handleResearch();
+        if (special)
+            secondSelection();
+        else
+            handleResearch();
     }
 
     private void goBack () {
@@ -479,17 +481,14 @@ public class BrowserController implements Initializable {
         if (page == 0)
             backBt.setDisable(true);
         forwardBt.setDisable(false);
-        handleResearch();
-    }
-
-    private void logout(MouseEvent event) {
-        Session.resetInstance();
-        Utils.changeScene("/it/unipi/dii/lsmd/paperraterapp/layout/login.fxml", event);
+        if (special)
+            secondSelection();
+        else
+            handleResearch();
     }
 
     private void setGridUsers() {
-        cardsGrid.getChildren().clear();
-        cardsGrid.getColumnConstraints().clear();
+        cleanGrid();
         cardsGrid.setHgap(20);
         cardsGrid.setVgap(20);
         cardsGrid.setPadding(new Insets(30,40,30,40));
@@ -499,8 +498,7 @@ public class BrowserController implements Initializable {
     }
 
     private void setGridPapers() {
-        cardsGrid.getChildren().clear();
-        cardsGrid.getColumnConstraints().clear();
+        cleanGrid();
         cardsGrid.setAlignment(Pos.CENTER);
         cardsGrid.setVgap(40);
         cardsGrid.setPadding(new Insets(30,40,30,100));
@@ -510,8 +508,7 @@ public class BrowserController implements Initializable {
     }
 
     private void setGridReadingList() {
-        cardsGrid.getChildren().clear();
-        cardsGrid.getColumnConstraints().clear();
+        cleanGrid();
         cardsGrid.setAlignment(Pos.CENTER);
         cardsGrid.setVgap(20);
         cardsGrid.setPadding(new Insets(30,40,30,160));
@@ -519,4 +516,12 @@ public class BrowserController implements Initializable {
         constraints.setPercentWidth(100);
         cardsGrid.getColumnConstraints().add(constraints);
     }
+
+    private void cleanGrid() {
+        cardsGrid.getColumnConstraints().clear();
+        while (cardsGrid.getChildren().size() > 0) {
+            cardsGrid.getChildren().remove(0);
+        }
+    }
+
 }
